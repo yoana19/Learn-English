7acb35ee939284e601d7c1c1c9861a6444273254
Yoana Stankova <yoana19st@gmail.com>
2019-03-04 19:02:56 +0200

    Update levels.php

--- levels.php
+++ levels.php
@@ -6,7 +6,144 @@
 <div id="triangle-topright2" class=" animated slideInRight"></div>
 <div id="triangle-topright3" class=" animated slideInRight"></div>
 <div id="info-section">
-    <h1 class=" animated zoomIn">ZISUZON</h1>
+    <h1 class=" animated zoomIn">
+
+        <?php
+if(isset($_GET['a1/1/1'])) {
+    
+    echo 'Introduction';
+    
+} else if (isset($_GET['a1/1/2'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445408" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+        
+} else if (isset($_GET['a1/1/3'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445419" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+}  else if (isset($_GET['a1/1/4'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445434" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/1/5'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445438" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/1/6'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445445" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/2/1'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445447" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/2/2'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445449" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/2/3'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445454" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/2/4'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445455" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/2/5'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445456" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/2/6'])) {
+    
+    echo '<iframe src="https://h5p.org/h5p/embed/445458" width="899" height="138" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>';
+    
+} else if (isset($_GET['a1/3/1'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/3/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/3/3'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/3/4'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/3/5'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/4/1'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/4/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/4/3'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/4/4'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/4/5'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/5/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/5/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/5/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/5/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/5/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/6/1'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/6/2'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/6/3'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/6/4'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/6/5'])) {
+    
+    echo '';
+    
+} else if (isset($_GET['a1/6/6'])) {
+    
+    echo '';
+    
+}
+?>
+    </h1>
 </div>
 <div id="triangle-bottomleft" class=" animated slideInLeft"></div>
 <div id="triangle-bottomleft2" class=" animated slideInLeft"></div>
