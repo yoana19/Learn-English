c9e4866850d4e9186a814aec19bccf609b4cf433
Yoana Stankova <yoana19st@gmail.com>
2019-03-19 23:25:46 +0200

    Update xp.inc.php

--- includes/xp.inc.php
+++ includes/xp.inc.php
@@ -1,18 +1,18 @@
 <?php
 
 if(isset($_POST['add-xp'])) {
-    
+
     require "dbh.inc.php";
-    
+
     $id1 = $row['idUsers'];
-	$id2 = $_SESSION['userId'];
-	$id3 = 6;
+  	$id2 = $_SESSION['userId'];
+  	$id3 = 6;
     mysqli_query($conn,"UPDATE users SET xpUsers=(xpUsers + 100) WHERE idUsers=".$id2."");
-	
+
     header("Location: ../profilepage.php");
     exit();
 
     mysqli_close($conn);
-	
+
 
 }
